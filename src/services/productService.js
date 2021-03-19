1ef112fd251528c9f6d62d07edbb0b9a01006a15
ramdannugraha <ramdannugraha08@gmail.com>
2021-03-19 10:14:48 +0700

    Second Commit : CUD

--- src/services/productService.js
+++ src/services/productService.js
@@ -50,17 +50,82 @@ async function getById(request, reply) {
 }
 
 async function create(request, reply) {
+    const {
+        nama,
+        harga,
+        warna,
+        kondisi,
+        deskripsi,
+    } = request.body;
+
+    let sql = `INSERT INTO product ( nama, harga, warna, kondisi, deskripsi, created_at) values(?,?,?,?,?)`;
+    const now = new Date();
+    let data = await new Promise((resolve) =>
+        connection.query(
+            sql,
+            [
+                nama,
+                harga,
+                warna,
+                kondisi,
+                deskripsi,
+                now,
+            ],
+            function (error, rows) {
+                if (error) {
+                    console.log(error);
+                    return res.badRequest(false, `${error}`, reply);
+                }
+
+                return rows.affectedRows > 0 ? resolve(rows) : resolve(false);
+            }
+        )
+    );
+
+    if (!data) {
+        return res.badData(data, "Data products can't add to table", reply);
+    }
+
+    return res.ok(data, "Data products added to table successfully", reply);
 
 }
 
 async function update(request, reply) {
+    const id = request.params.id;
+    const now = new Date();
+    let queryBuilder = "";
+    let arrValue = [];
+    const products = request.body;
+    await Object.keys(products).forEach(function (key) {
+        queryBuilder = " " + queryBuilder + key + " = ? ,";
+        arrValue.push(products[key]);
+    });
+    let sql =
+        "UPDATE product SET " + queryBuilder + "updated_at = ? where id = ?";
+    arrValue.push(now);
+    arrValue.push(id);
+    let data = await new Promise((resolve) =>
+        connection.query(sql, arrValue, function (error, rows) {
+            if (error) {
+                console.log(error);
+                return res.badRequest("", `${error}`, reply);
+            }
+
+            return rows.affectedRows > 0 ? resolve(true) : resolve(false);
+        })
+    );
+    if (!data) {
+        return res.badData(data, "Data products can't update to table", reply);
+    }
+
+    return res.ok(data, "Data products updated to table successfully", reply);
 
 }
 
 
 async function destroy(request, reply) {
     let id = request.params.id;
-    let sql = `DELETE FROM banner WHERE id = ?`;
+    let sql = `DELETE FROM product WHERE id = ?`;
 
     let data = await new Promise((resolve) =>
         connection.query(sql, [id], function (error, rows) {
@@ -74,10 +139,10 @@ async function destroy(request, reply) {
     );
 
     if (!data) {
-        return res.badData(data, "Data Banner can't delete from table", reply);
+        return res.badData(data, "Data Product can't delete from table", reply);
     }
 
-    return res.ok(data, "Data Banner deleted from table successfully", reply);
+    return res.ok(data, "Data Product deleted from table successfully", reply);
 }
 
 module.exports = {
